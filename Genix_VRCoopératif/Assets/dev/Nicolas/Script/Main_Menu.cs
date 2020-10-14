15542dfb0533bf0417149e9969fe37299f6c113b
Unknown <nicolas.faqir@hotmail.fr>
2020-10-14 17:54:50 +0200

    Main menu canvas with placeholder

---
+++ Genix_VRCoopératif/Assets/dev/Nicolas/Script/Main_Menu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main_Menu : MonoBehaviour
+{
+    public GameObject newCanvas;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChangeCanvas(GameObject newCanvas)
+    {
+        gameObject.setActive(false);
+        newCanvas.setActive(true);
+    }
+}
